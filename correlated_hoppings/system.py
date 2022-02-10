2581cf826d93ff9dfa4171f18903ae359fe2f8f3
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-02-10 16:22:21 +0100

    implementation of total spin operator

--- correlated_hoppings/system.py
+++ correlated_hoppings/system.py
@@ -1,5 +1,6 @@
 import numpy as np
 import quspin
+import scipy.sparse.linalg
 from typing import Any, List, Tuple
 
 
@@ -31,6 +32,10 @@ def simple_square_lattice_edges(shape: Tuple[int, int]) -> List[Tuple[int, int]]
     edges = sorted(map(order_edge, edges))
     assert are_edges_ordered(edges)
     return edges
+
+
+def square_2x2() -> SquareLattice:
+    return SquareLattice([(0, 1), (2, 3), (0, 2), (1, 3)])
 
 
 def square_3x3() -> SquareLattice:
@@ -84,6 +89,58 @@ def normal_hubbard(J, U, edges, basis):
     return quspin.operators.hamiltonian(static_part, [], basis=basis, dtype=np.float64)
 
 
+def measure_total_spin(v, basis):
+    indices = [(i, j) for i in range(basis.N // 2) for j in range(basis.N // 2)]
+
+    static_part = [
+        # S_x S_x
+        # ["--|++", [[-1, i, j, i, j] for i, j in indices]],
+        # ["++|--", [[-1, i, j, i, j] for i, j in indices]],
+        ["-+|+-", [[1, i, j, i, j] for i, j in indices]],
+        ["+-|-+", [[1, i, j, i, j] for i, j in indices]],
+        # S_y S_y
+        # ["--|++", [[1, i, j, i, j] for i, j in indices]],
+        # ["++|--", [[1, i, j, i, j] for i, j in indices]],
+        ["-+|+-", [[1, i, j, i, j] for i, j in indices]],
+        ["+-|-+", [[1, i, j, i, j] for i, j in indices]],
+        # S_z S_z
+        ["nn|", [[1, i, j] for i, j in indices]],
+        ["n|n", [[-1, i, j] for i, j in indices]],
+        ["n|n", [[-1, i, j] for i, j in indices]],
+        ["|nn", [[1, i, j] for i, j in indices]],
+    ]
+    S = quspin.operators.hamiltonian(static_part, [], basis=basis, dtype=np.complex128).tocsr()
+    r = float(np.vdot(v, S.dot(v)))
+    return 0.5 * (-1 + np.sqrt(1 + r))
+
+
+def dehollain_2020():
+    number_sites = 4
+    lattice = square_2x2()
+    U = 2900
+
+    def sweep(basis):
+        table = []
+        for t in np.linspace(0, 200, num=11):
+            h = make_hamiltonian(-t, -t, 0, U, lattice.edges, basis).tocsr()
+            # h = normal_hubbard(t, U, lattice.edges, basis).tocsr()
+            e, v = scipy.sparse.linalg.eigsh(h, k=2, tol=1e-14, which="SA")
+            if t == 20:
+                v[np.abs(v) < 1e-14] = 0
+                print(v)
+            print(measure_total_spin(v[:, 0], basis), measure_total_spin(v[:, 1], basis))
+            table.append((t, e + 2 * t))
+        return table
+
+    basis = quspin.basis.spinful_fermion_basis_general(number_sites, Nf=(2, 1))
+    print(basis)
+    table = sweep(basis)
+    print(table)
+
+    # basis = quspin.basis.spinful_fermion_basis_general(number_sites, Nf=(3, 0))
+    # table = sweep(basis)
+    # print(table)
+
 def run_tests():
     # 0 1 2
     # 3 4 5
@@ -128,7 +185,7 @@ def run_tests():
     assert np.all(h1.indptr == h2.indptr)
 
     # print(basis)
-    # hamiltonian = 
+    # hamiltonian =
     # print(hamiltonian)
     # energy, ground_state = hamiltonian.eigsh(k=1, which="SA")
     # print(energy)
@@ -142,4 +199,5 @@ def run_tests():
 
 
 if __name__ == "__main__":
-    run_tests()
+    dehollain_2020()
+    # run_tests()
