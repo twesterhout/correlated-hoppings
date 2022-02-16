5c0a7bd449bd42dc8d18e928e8e7373cee52b50e
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-02-16 15:25:42 +0100

    Rename and add more sizes

--- correlated_hoppings/system.py
+++ correlated_hoppings/system.py
@@ -36,12 +36,72 @@ def simple_square_lattice_edges(shape: Tuple[int, int]) -> List[Tuple[int, int]]
     return edges
 
 
-def square_2x2() -> SquareLattice:
+def square_4() -> SquareLattice:
     return SquareLattice(4, [(0, 1), (2, 3), (0, 2), (1, 3)])
 
 
-def square_3x3() -> SquareLattice:
+def square_5() -> SquareLattice:
+    #          0  1
+    #       2  3  4
+    #
+    #             0  1
+    #          2  3  4  0  1
+    #          0  1  2  3  4
+    #       2  3  4  0  1
+    #             2  3  4
+    #
+    # fmt: off
+    return SquareLattice(5, [(0, 1), (0, 3), (1, 2), (1, 4), (2, 3),
+                             (0, 2), (3, 4), (1, 3), (0, 4), (2, 4)])
+    # fmt: on
+
+
+def square_8() -> SquareLattice:
+    #          0  1
+    #       2  3  4  5
+    #          6  7
+    #
+    #                    0  1
+    #                 2  3  4  5
+    #              0  1  6  7  0  1
+    #           2  3  4  5  2  3  4  5
+    #              6  7  0  1  6  7
+    #                 2  3  4  5
+    #                    6  7
+    #
+    # fmt: off
+    return SquareLattice(8, [(0, 1), (0, 3), (1, 6), (1, 4),
+                             (2, 3), (1, 2), (3, 4), (3, 6),
+                             (4, 5), (4, 7), (2, 5), (0, 5),
+                             (6, 7), (5, 6), (0, 7), (2, 7)])
+    # fmt: on
+
+
+def square_9() -> SquareLattice:
     return SquareLattice(9, simple_square_lattice_edges((3, 3)))
+
+
+def square_10() -> SquareLattice:
+    #          0  1  2
+    #          3  4  5
+    #       6  7  8  9
+    #
+    #                        0  1  2
+    #                        3  4  5  0  1  2
+    #                     6  7  8  9  3  4  5
+    #                     0  1  2  6  7  8  9
+    #                     3  4  5  0  1  2
+    #                  6  7  8  9  3  4  5
+    #                           6  7  8  9
+    #
+    #
+    # fmt: off
+    return SquareLattice(10, [(0, 1), (0, 3), (1, 2), (1, 4),
+                              (2, 6), (2, 5), (3, 4), (3, 7),
+                              (4, 5), (4, 8), (0, 5), (5, 9),
+                              (6, 7), (0, 6), (7, 8), (1, 7),
+                              (8, 9), (2, 8), (3, 9), (6, 9)])
+    # fmt: on
 
 
 def unconjugated_hamiltonian(
@@ -280,6 +340,10 @@ def dehollain_2020():
     # table = sweep(basis)
     # print(table)
 
+
+def energy_scaling():
+    for lattice in [square_4(), square_5(), square_8(), square_9(), square_10()]:
+        pass
 
 def run_tests():
     # 0 1 2
